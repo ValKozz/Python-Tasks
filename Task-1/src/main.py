5f2298c0872567b62f9ecfb81abdf52dfde0a47b
valkozz <v.kozarev98@gmail.com>
2024-11-30 18:21:10 +0200

    Got rid of unnecessary code and simplified formating and printing. Implemented single city lookup.

--- Task-1/src/main.py
+++ Task-1/src/main.py
@@ -8,13 +8,6 @@ API_KEY = os.environ['API_KEY']
 EXCLUDE_FROM_RESPONSE = "minutely,hourly,daily,alerts"
 UNITS = "metric"
 
-def collect_cities():
-    cities = []
-    while len(cities) < 5:
-        cities.append(get_random_city())
-
-    for city in cities:
-        print(city)
 
 def get_random_city():
     longitude = randrange(-180, 181)
@@ -27,17 +20,28 @@ def get_random_city():
         latitude = randrange(-90, 91)
         city_data_full = get_data(latitude, longitude)
 
-    format_response = {
-        "name" : city_data_full["name"],
-        "country" : city_data_full["sys"]["country"],
-        "humidity" : city_data_full["main"]["humidity"],
-        "temp" : city_data_full["main"]["temp"],
-        "weather_desc" : city_data_full["weather"][0]["description"],
-        "clouds" : city_data_full["weather"][0]["main"],
-        "clouds_percent" : city_data_full["clouds"]["all"]
-         }
+    return format_and_print(city_data_full)
 
-    return format_response
+
+def format_and_print(city_data_full):
+        name = city_data_full["name"]
+        country = city_data_full["sys"]["country"]
+        humidity = city_data_full["main"]["humidity"]
+        temp = city_data_full["main"]["temp"],
+        weather_desc = city_data_full["weather"][0]["description"]
+        clouds = city_data_full["weather"][0]["main"]
+        clouds_percent = city_data_full["clouds"]["all"]
+
+        format_print =\
+f"""
+Name: {name}, {country}
+    Current Temperature: {temp} C
+    Humidity: {humidity}%
+    Clouds:  {clouds_percent}% of sky covered by clouds
+    Weather : {clouds}
+    Weather description: {weather_desc}
+"""
+        print(format_print)
 
 def get_data(latitude, longitude):
     city_lookup_raw = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&units={UNITS}&lon={longitude}&appid={API_KEY}")
@@ -51,22 +55,23 @@ def get_by_name(city_name):
     city_lookup_raw.raise_for_status()
     city_repsonse = city_lookup_raw.json()
 
-    format_response = {
-        "lat" : city_repsonse[0]["lat"],
-        "lon" : city_repsonse[0]["lon"],
-         }
+    latitude = city_repsonse[0]["lat"]
+    longitude = city_repsonse[0]["lon"]
 
-    return format_response
+    city_data_full = get_data(latitude, longitude)
+
+    return format_and_print(city_data_full)
 
 def main():
     option = input("City lookup (s)\nGet 5 random (r)\nQuit (q)\nPlease enter the desired option:\n")
 
-    if option == "s":
+    if option == "s".lower():
         name = input("City: ")
         get_by_name(name)
-    elif option == "r":
-        collect_cities()
-    elif option == "q":
+    elif option == "r".lower():
+        for i in range (0, 5):
+            get_random_city()
+    elif option == "q".lower():
         quit()
     else:
         print("Invalid input. Try again\n")
